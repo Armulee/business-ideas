4d8b82b0159cfc7d81232939235fb0335fc072d6
Armulee <makkaroon.thanawat@gmail.com>
2025-06-28 02:15:51 +0700

    Beta Version

--- components/post/types.ts
+++ components/post/types.ts
@@ -2,13 +2,13 @@ import { ICommentPopulated } from "@/database/Comment"
 import { IPostPopulated } from "@/database/Post"
 import { IProfile } from "@/database/Profile"
 import { IReplyPopulated } from "@/database/Reply"
-import { WidgetType } from "@/database/Widget"
+import { Widget } from "@/database/Widget"
 
 export interface PostData {
     post: IPostPopulated | undefined
     comments: ICommentPopulated[] | undefined
     replies: Record<string, IReplyPopulated[]> | undefined
-    widgets: WidgetType[] | undefined
+    widgets: Widget[] | undefined
     profile?: IProfile | undefined
 }
 
